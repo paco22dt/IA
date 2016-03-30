4fa40fb653085c05b5d70ec968bc1bdc0aa3ac33
Francisco Manjón Gottlob <franciscogottlob@hotmail.com>
2016-03-30 05:20:34 -0600

    Added project

---
+++ BookNext/src/design/main.java
@@ -0,0 +1,15 @@
+package design;
+
+import javax.swing.JOptionPane;
+import java.awt.FlowLayout;
+import javax.swing.*;
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Interface().setVisible(true);
+	}
+
+}
---
+++ engineBN/src/classes/engine.scala
@@ -0,0 +1,11 @@
+package classes
+
+class engine {
+  
+  def Prueba(hola:Int):Int=
+  {
+    var sum:Int=0
+    sum=hola+hola
+    return sum
+  }
+}
