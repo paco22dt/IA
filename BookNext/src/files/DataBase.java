c27f1c41b55194fef6e33037f600c956b89c829c
Francisco Manjón Gottlob <franciscogottlob@hotmail.com>
2016-05-08 15:08:45 -0600

    DataBase

--- BookNext/src/files/DataBase.java
+++ BookNext/src/files/DataBase.java
@@ -58,12 +58,12 @@ public class DataBase {
 				if(resultSet.next())
 				{
 					query="update categoria_usuario set cantidad = cantidad + 1 where id_usuario = "+id_usuario + "and categoria = " + categorias[i];
-					statement.executeQuery(query);
+					statement.executeUpdate(query);
 				}
 				else
 				{
 					query = "insert into categoria_usuario (categoria,cantidad,id_usuario) values (" + categorias[i]+ ",1," + id_usuario + ")";
-					statement.executeQuery(query);
+					statement.executeUpdate(query);
 				}
 			}
 			for(int i=0; i<palabras_clave.length; i++)
@@ -73,12 +73,12 @@ public class DataBase {
 				if(resultSet.next())
 				{
 					query="update palabra_claves_usuario set cantidad = cantidad + 1 where id_usuario = "+id_usuario + "and palabra_clave = " + palabras_clave[i];
-					statement.executeQuery(query);
+					statement.executeUpdate(query);
 				}
 				else
 				{
 					query = "insert into palabra_claves_usuario (palabra_clave,cantidad,id_usuario) values (" + palabras_clave[i]+ ",1," + id_usuario + ")";
-					statement.executeQuery(query);
+					statement.executeUpdate(query);
 				}
 			}
 			for(int i=0; i<autores.length; i++)
@@ -88,12 +88,12 @@ public class DataBase {
 				if(resultSet.next())
 				{
 					query="update autor_usuario set cantidad = cantidad + 1 where id_usuario = "+id_usuario + "and autor = " + autores[i];
-					statement.executeQuery(query);
+					statement.executeUpdate(query);
 				}
 				else
 				{
 					query = "insert into autor_usuario (autor,cantidad,id_usuario) values (" + autores[i]+ ",1," + id_usuario + ")";
-					statement.executeQuery(query);
+					statement.executeUpdate(query);
 				}
 			}
 		}
@@ -103,22 +103,24 @@ public class DataBase {
 		}
 	}
 	
-	public boolean Login(String username, String password) throws Exception{
+	public int Login(String username, String password) throws Exception{
 		try
 		{
-			String query = "SELECT * FROM categoria_usuario WHERE username = " + username + "and password = " + password;
+			String query = "SELECT * FROM usuario WHERE username = '" + username + "' and password = '" + password + "'";
 			statement = connect.createStatement();
 			resultSet = statement.executeQuery(query);
 			if(resultSet.next())
 			{
-				return true;
+				int id = resultSet.getInt("id_usuario");
+				return id;
 			}
-			return false;
+			return 0;
 		}
 		catch (Exception e)
 		{
 			throw e;
 		}
+		
 	}
 	
 	public void InsertUsuario(String username, String password)throws Exception
@@ -127,7 +129,7 @@ public class DataBase {
 		{
 			String query = "insert into usuario (username,password) values (" + username + "," + password + ")";
 			statement = connect.createStatement();
-			statement.executeQuery(query);
+			statement.executeUpdate(query);
 		}
 		catch (Exception e)
 		{
@@ -258,6 +260,65 @@ public class DataBase {
 		}
 	}
 	
+	public void IngresarCategorias(int id_usuario, String[] categorias) throws Exception
+
+	{
+		for(int i=0; i<categorias.length; i++)
+		{
+			String query = "insert into categoria_usuario (categoria,cantidad,id_usuario) values ('" + categorias[i]+ "',1," + id_usuario + ")";
+			try {
+				statement = connect.createStatement();
+				statement.executeUpdate(query);
+			} catch (SQLException e) {
+				throw e;
+			}
+		}
+	}
+	public void IngresarAutores(int id_usuario, String[] autores) throws Exception
+	{
+		for(int i=0; i<autores.length; i++)
+		{
+			String query = "insert into autor_usuario (autor,cantidad,id_usuario) values ('" + autores[i]+ "',1," + id_usuario + ")";
+			try {
+				statement = connect.createStatement();
+				statement.executeUpdate(query);
+			} catch (SQLException e) {
+				throw e;
+			}
+		}
+	}
+	public void IngresarPalabrasClaves(int id_usuario, String[] palabras_clave) throws Exception
+	{
+		for(int i=0; i<palabras_clave.length; i++)
+		{
+			String query = "insert into palabra_claves_usuario (palabra_clave,cantidad,id_usuario) values (" + palabras_clave[i]+ ",1," + id_usuario + ")";
+			try {
+				statement = connect.createStatement();
+				statement.executeUpdate(query);
+			} catch (SQLException e) {
+				throw e;
+			}
+		}
+	}
 	
+	public int[] CalcularLibros() throws Exception
+	{
+		String query = "select id_libro from libro where id_libro = ANY(SELECT id_libro from usuario_libro WHERE id_usuario=3)";
+		try
+		{
+			statement = connect.createStatement();
+			resultSet = statement.executeQuery(query);
+			ArrayList<Integer> resultados = new ArrayList();
+			while(resultSet.next())
+			{
+				
+			}
+			return resultados.stream().mapToInt(i -> i).toArray();
+		}
+		catch (Exception ex)
+		{
+			throw ex;
+		}
+	}
 	
 }
---
+++ BookNext/src/interfaz/Recommendation2.java
@@ -0,0 +1,69 @@
+package interfaz;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.border.TitledBorder;
+import java.awt.FlowLayout;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+
+public class Recommendation2 extends JPanel {
+	private JTable table;
+	private JTable table_1;
+	private JTable table_2;
+
+	/**
+	 * Create the panel.
+	 */
+	public Recommendation2() {
+		setBorder(new TitledBorder(null, "Recommendation", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		setBackground(new Color(152, 251, 152));
+		setLayout(new GridLayout(2, 2, 20, 20));
+		
+		JLabel lblSeleccioneElTipo = new JLabel("Seleccione el tipo de recommendacion que necesita");
+		lblSeleccioneElTipo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		add(lblSeleccioneElTipo);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		add(scrollPane_1);
+		
+		table_2 = new JTable();
+		scrollPane_1.setViewportView(table_2);
+		table_2.setColumnSelectionAllowed(true);
+		table_2.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		
+		JButton btnGenerarRecomendacin = new JButton("Generar Recomendaci\u00F3n");
+		btnGenerarRecomendacin.setFont(new Font("Tahoma", Font.BOLD, 16));
+		add(btnGenerarRecomendacin);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane);
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null},
+				{null, "", null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"T\u00EDtulo", "Autor(es)", "Edici\u00F3n", "A\u00F1o de publicaci\u00F3n", "Resumen", "Categor\u00EDa(s)", "Palabra(s) clave"
+			}
+		));
+		table.setColumnSelectionAllowed(true);
+		table.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		
+		scrollPane.setViewportView(table);
+		
+
+	}
+
+}
